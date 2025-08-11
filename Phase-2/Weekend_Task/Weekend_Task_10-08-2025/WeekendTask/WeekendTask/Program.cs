cc421be509730fcce2bd915de3b54089f910680c
yuvaraj3333 <yuvisasi333@gmail.com>
2025-08-11 16:00:58 +0530

    Commit changes successfully

---
+++ Phase-2/Weekend_Task/Weekend_Task_10-08-2025/WeekendTask/WeekendTask/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace FetchPostsApp
+{
+    public class Post
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string url = "https://jsonplaceholder.typicode.com/posts";
+
+            try
+            {
+                List<Post> posts = await FetchPostsAsync(url);
+
+                foreach (var post in posts)
+                {
+                    Console.WriteLine($"Post ID: {post.Id}");
+                    Console.WriteLine($"User ID: {post.UserId}");
+                    Console.WriteLine($"Title: {post.Title}");
+                    Console.WriteLine($"Body: {post.Body}");
+                    Console.WriteLine(new string('-', 50));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+        static async Task<List<Post>> FetchPostsAsync(string url)
+        {
+            using HttpClient client = new HttpClient();
+
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode(); 
+
+            string json = await response.Content.ReadAsStringAsync();
+
+            return JsonSerializer.Deserialize<List<Post>>(json, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+        }
+    }
+}
