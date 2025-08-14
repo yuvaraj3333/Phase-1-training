1556601966b075ef4fbbbe336d3fffe366d69e53
yuvaraj3333 <yuvisasi333@gmail.com>
2025-08-14 09:17:31 +0530

    Commit changes successfully

---
+++ Phase-2/Daily_Task/Day7_Daily_Task_13-08-2025/PainterApp/PainterApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PainterLib;
+
+var services = new ServiceCollection();
+
+// Choose which tool to inject
+services.AddTransient<IPaintTool, PaintBrush>();
+services.AddTransient<Painter>();
+
+var provider = services.BuildServiceProvider();
+var painter = provider.GetRequiredService<Painter>();
+
+Console.WriteLine(painter.StartPainting("wall"));
---
+++ Phase-2/Daily_Task/Day7_Daily_Task_13-08-2025/PainterApp/PainterApp/ServiceCollection.cs
@@ -0,0 +1,6 @@
+﻿internal class ServiceCollection
+{
+    public ServiceCollection()
+    {
+    }
+}
